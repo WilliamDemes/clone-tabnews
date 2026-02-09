21ec29c136d93bd8d605cd2d2a9473a6dc04b250
WilliamDemes <williamdemes58@gmail.com>
2026-02-09 03:15:49 +0000

    meu primeiro projeto escalável

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>IEstou aqui para ajudar no curso.dev</h1>
+    return <h1>Esse é o primeiro passo do grande programador marajoara que vou ser, no lugar onde a tecnologia tem dificuldade de domar serei o discipulo que a pregarei.</h1>
 }
 
 export default Home;
