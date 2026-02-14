d5cafb77c6b114fc09f76f8628f7c4a11a457466
WilliamDemes <williamdemes58@gmail.com>
2026-02-14 18:33:56 +0000

    adiciona scripts `lisnt:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Um projeto prárico para treinar programação
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
 function Home() {
-    return <h1>Esse é o primeiro passo do grande programador marajoara que vou ser, no lugar onde a tecnologia tem dificuldade de domar serei o discipulo que a pregarei.</h1>
+  return (
+    <h1>
+      Esse é o primeiro passo do grande programador marajoara que vou ser, no
+      lugar onde a tecnologia tem dificuldade de domar serei o discipulo que a
+      pregarei.
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("identação errada");
 }
 
 export default Home;
